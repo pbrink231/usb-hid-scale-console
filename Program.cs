7d8d77d292a9c663bcb02ad3cc5de0189f341e50
Peter Brink <pbrink231@gmail.com>
2021-03-18 13:28:07 -0400

    hidlibrary added and kg units with conversions

--- Program.cs
+++ Program.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿//Program.cs
+using System;
+using System.Threading;
+using HidLibrary;
+using Scale;
+using System.Timers;
 
-namespace usb_scale_console_test
+namespace ScaleReader
 {
     class Program
     {
-        static void Main(string[] args)
+        private static System.Timers.Timer aTimer;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            aTimer = new System.Timers.Timer(1000);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.Enabled = true;
+
+            Console.WriteLine("Let the scale do it's thing...");
+            Console.ReadLine();
+            
+        }
+
+        static void OnTimedEvent(object sender, System.EventArgs e)
+        {
+            decimal? weightInLb, weightInG, weightInOz, weightInKg;
+            bool? isStable;
+
+            USBScale s = new USBScale();
+            s.Connect();
+
+            if (s.IsConnected)
+            {
+                s.GetWeight(out weightInG, out weightInOz, out weightInLb, out weightInKg, out isStable);
+                s.DebugScaleData();
+                Console.WriteLine("Weight: {0:0.00} g", weightInG);
+                Console.WriteLine("Weight: {0:0.00} oz", weightInOz);
+                Console.WriteLine("Weight: {0:0.00} LBS", weightInLb);
+                Console.WriteLine("Weight: {0:0.00} KG", weightInKg);
+                Console.WriteLine("Stable?: {0}", isStable);
+                Console.WriteLine("--------------------- {0}", DateTime.Now);
+                s.Disconnect(); 
+            }
+            else
+            {
+                Console.WriteLine("No Scale Connected.");
+            }
         }
     }
 }
--- scale.cs
+++ scale.cs
@@ -43,17 +43,24 @@ namespace Scale
 
             HidDevice[] hidDevice;
 
+            // product/vendor ID number for Fairbanks Scales SCB-R9000
+            hidDevice = HidDevices.Enumerate(0x0b67, 0x555e).ToArray();
+            Console.WriteLine("hidDevice Fairbanks len {0}", hidDevice.Length);
+
+            if (hidDevice.Length > 0)
+                return hidDevice;
+
 
             // product/vendor ID number Dymo 25lb Postal Scale
             hidDevice = HidDevices.Enumerate(0x0922, 0x8003).ToArray();
-            Console.WriteLine("hidDevice len {0}", hidDevice.Length);
+            Console.WriteLine("hidDevice Dymo len {0}", hidDevice.Length);
 
             if (hidDevice.Length > 0)
                 return hidDevice;
 
             // product/vendor ID number for Elane USB 30
             hidDevice = HidDevices.Enumerate(0x7B7C, 0x0204).ToArray();
-            Console.WriteLine("hidDevice len {0}", hidDevice.Length);
+            Console.WriteLine("hidDevice Elane len {0}", hidDevice.Length);
 
             if (hidDevice.Length > 0)
                 return hidDevice;
@@ -109,13 +116,14 @@ namespace Scale
             }
         }
 
-        public void GetWeight(out decimal? weightInLb, out decimal? weightInG, out decimal? weightInOz, out bool? isStable)
+        public void GetWeight(out decimal? weightInG, out decimal? weightInOz, out decimal? weightInLb, out decimal? weightInKg, out bool? isStable)
         {
             decimal? weight;
             weight = null;
             weightInG = null;
-            weightInLb = null;
             weightInOz = null;
+            weightInLb = null;
+            weightInKg = null;
             isStable = false;
 
             if (scale.IsConnected)
@@ -135,24 +143,37 @@ namespace Scale
 
                 Console.WriteLine("scale status {0}", inData.Data[1]);
 
-
                 switch (Convert.ToInt16(inData.Data[2]))
                 {
-                    case 2:  // Scale reading in g
+                    case 2:  // g
                         weight = (Convert.ToDecimal(inData.Data[4]) + Convert.ToDecimal(inData.Data[5]) * 256);
                         weightInG = weight;
-                        weightInLb = weight * (decimal?)0.00220462;
                         weightInOz = weight * (decimal?)0.035274;
-                        //weightInG = weightInG * (decimal?)0.625;
+                        weightInLb = weight * (decimal?)0.00220462;
+                        weightInKg = weight * (decimal?)0.001;
+                        break;
+                    case 3:  // kg
+                        weight = (Convert.ToDecimal(inData.Data[4]) + Convert.ToDecimal(inData.Data[5]) * 256) / 100;
+                        weightInG = weight * (decimal?)1000;
+                        weightInOz = weight * (decimal?)35.274;
+                        weightInLb = weight * (decimal?)2.20462;
+                        weightInKg = weight;
                         break;
                     case 11: // Ounces
                         weight = (Convert.ToDecimal(inData.Data[4]) + Convert.ToDecimal(inData.Data[5]) * 256) / 10;
-                        weightInLb = weight * (decimal?)0.0625;
-                        weightInOz = weight;
                         weightInG = weight * (decimal?)28.3495;
+                        weightInOz = weight;
+                        weightInLb = weight * (decimal?)0.0625;
+                        weightInKg = weight * (decimal?)0.0283495;
                         break;
                     case 12: // Pounds
                         // already in pounds, do nothing
+                        weight = (decimal?)BitConverter.ToInt16(new byte[] { inData.Data[4], inData.Data[5] }, 0) *
+                                    Convert.ToDecimal(Math.Pow(10, (sbyte)inData.Data[3]));
+                        weightInG = weight * (decimal?)453.592;
+                        weightInOz = weight * (decimal?)16;
+                        weightInLb = weight;
+                        weightInKg = weight * (decimal?)0.453592;
                         break;
                 }
                 isStable = inData.Data[1] == 0x4;
